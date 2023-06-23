fc08e844a232e536b2ea6205c1af82847d93158e
Johnny <aguguaj@hotmail.com>
2023-06-23 15:23:22 -0500

    Finsh creating scooter class

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,14 @@
 class Scooter{
   // scooter code here
+  static nextSerial = 1;
+
+  constructor(station) {
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial++;
+    this.charge = 100;
+    this.isBroken = false;
+  }
 }
 
 
